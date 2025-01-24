142379eeb0199c489949455efd34966e5f1119dd
LuizSoares0408 <luiz.h.g.soares@outlook.com>
2025-01-24 20:43:54 -0300

    Opção de saída
    Incluso no sistema a opção de saída para o usuário.

--- Cartório.c
+++ Cartório.c
@@ -105,48 +105,53 @@
 			system("pause");
 		}
 	}
-	int main ()
+int main ()
 {
-							int opcao=0; //Definindo variáveis
-							int laco=1;
+	int opcao=0; //Definindo variáveis
+	int laco=1;
 							
-							for(laco=1;laco=1;)
-							{
-								system("cls");
-						
-								setlocale(LC_ALL, "Portuguese"); //Definindo a linguagem	
+	for(laco=1;laco=1;)
+	{
+		system("cls");
 
-       							printf("### Cartório da EBAC ###\n\n");
+		setlocale(LC_ALL, "Portuguese"); //Definindo a linguagem	
 
-								printf ("Escolha a opção desejada do menu: \n\n"); //Início do menu
-								printf("\t1 - Registrar nomes\n");
-								printf("\t2 - Consultar nomes\n");
-								printf("\t3 - Deletar nomes\n\n");
-								printf("Opção:"); //Fim do menu
-							
-								scanf("%d", &opcao); // Armazenando a escolha do usuário
-							
-								system("cls"); //Responsável por limpar a tela
-								
-								switch(opcao) //Inicio da seleção do menu
-								{
-									case 1:
-									registro(); //chamada de funções
-									break;
-									
-									case 2:
-									consulta();
-									break;
-									
-									case 3:
-									deletar();
-									break;								
-									
-									default:
-									printf("Essa opção não esta disponível\n");
-									system("pause");
-									break;	
-								} //fim da seleção
-							
-							}				
+       	printf("### Cartório da EBAC ###\n\n");
+		printf ("Escolha a opção desejada do menu: \n\n"); //Início do menu
+		printf("\t1 - Registrar nomes\n");
+		printf("\t2 - Consultar nomes\n");
+		printf("\t3 - Deletar nomes\n\n");
+		printf("\t4 - Sair do sistema\n\n");
+		printf("Opção:"); //Fim do menu
+
+		scanf("%d", &opcao); // Armazenando a escolha do usuário
+
+		system("cls"); //Responsável por limpar a tela
+	
+		switch(opcao) //Inicio da seleção do menu
+		{
+			case 1:
+			registro(); //chamada de funções
+			break;
+	
+			case 2:
+			consulta();
+			break;
+	
+			case 3:
+			deletar();
+			break;	
+			
+			case 4:
+			printf("Obrigado por utilizar o sistema!\n");
+			return 0;
+			break;							
+	
+			default:
+			printf("Essa opção não esta disponível\n");
+			system("pause");
+			break;	
+			} //fim da seleção
+	
+		}				
 }
